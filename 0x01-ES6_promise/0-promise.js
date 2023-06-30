4c57c850d72a3d6b279074d7e22e25d4e8b15024
Fara078 <faradokorie@gmail.com>
2023-06-30 03:54:23 +0100

    just updated

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,9 +1,3 @@
-// 0-promise.js
-function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    // Make API request or perform any asynchronous operation here
-    resolve("API response")
-  });
+export default function getResponseFromAPI() {
+  return new Promise(() => {});
 }
-
-export default getResponseFromAPI;
